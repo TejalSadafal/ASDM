214c0fb59af50f331330ed6d4de9ebb5663e0311
TejalSadafal <50069421+TejalSadafal@users.noreply.github.com>
2019-04-29 20:40:08 +0530

    Update server.js

--- dbdanode-master/server.js
+++ dbdanode-master/server.js
@@ -19,10 +19,10 @@ app.get('/hello', function (req, res) {
 });
 
  
-var server = app.listen(8090, function () {
+var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://localhost:8090", host, port)
+  console.log("Example app listening at http://localhost:8081", host, port)
 })
